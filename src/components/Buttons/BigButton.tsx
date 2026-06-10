d58b2225470ff27acb3b76cd3b61ba401d205a67
Jonah Butler <jonahbutler6@gmail.com>
2026-06-09 23:07:51 -0400

    changes to big button

--- src/components/Buttons/BigButton.tsx
+++ src/components/Buttons/BigButton.tsx
@@ -4,7 +4,9 @@ import '../../css/BigButton.css';
 type BigButtonProps = {
   icon?: ReactNode;
   header: string;
-  description?: string;
+  description?: ReactNode;
+  orientation?: 'row' | 'col';
+  size?: 'sm' | 'md' | 'full';
   onClick: () => void;
 };
 
@@ -13,10 +15,12 @@ export default function BigButton({
   header,
   description,
   onClick,
+  orientation = 'row',
+  size = 'full',
 }: BigButtonProps) {
   return (
-    <div onClick={() => onClick()} className="big-button">
-      <div className="big-button__container flex f-gap4">
+    <div onClick={() => onClick()} className={`big-button ${size}`}>
+      <div className={`big-button__container flex f-gap4 ${orientation}`}>
         {icon ? <section className="left">{icon}</section> : null}
         <section className="right">
           <h3>{header}</h3>
